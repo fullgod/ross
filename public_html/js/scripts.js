c8a721764f2389ee84e42ec7dd857f289181b21b
fullgod <fullgod@gmail.com>
2016-02-16 21:55:14 +0300

    drop

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -1,6 +1,6 @@
 (function($){
 	$(document).ready(function(){
-		$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+		$('ul.dropdown-menu [data-toggle=dropdown]').on('hover', function(event) {
 		event.preventDefault(); 
 		event.stopPropagation(); 
 		$(this).parent().siblings().removeClass('open');
