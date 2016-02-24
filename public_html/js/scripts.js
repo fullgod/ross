53e064f9825d7840fe3abff022dea72c0f85ee3c
fullgod <fullgod@gmail.com>
2016-02-24 03:44:42 +0300

    123

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -1,25 +1,38 @@
 (function($){
-	$(document).ready(function(){
-		$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+
+    if ($("ul.dropdown-menu [data-toggle=dropdown]")){
+    alert("Элемент есть");
+    }
+    else { alert("Элемента нет");}
+    
+     $('ul.dropdown-toggle').css( "background", "yellow" );
+
+$(document).ready(function(){
+  
+		$('ul.dropdown-toggle [data-toggle=dropdown]').on('click', function(event) {
 		event.preventDefault(); 
 		event.stopPropagation(); 
-		$(this).parent().siblings().removeClass('open');
-		$(this).parent().toggleClass('open');
-		
-		var menu = $(this).parent().find("ul");
-
-		if ($menu.hasClass('dropdown-menu')) {
-            $menu.css('margin-right', $menu.outerWidth() / -2);
-        }
-		
-		var newpos = menu.width();
+		//$(this).parent().siblings().removeClass('open');
+		//$(this).parent().toggleClass('open');
+        $(this).parent().css( "background", "yellow" );
+        
+        $(this).parent().addClass('open');
+        var menu = $(this).parent().find("ul");
+        var menupos = menu.offset();
+        if ($menu.hasClass('dropdown-menu')) {
+            menu.css('margin-right', $menu.outerWidth() / -2);
+        } 
+        var newpos = menu.width();
 		menu.css({left: newpos});
 
-
 		});
+        
+	
 	});
+    
+    
+    
 })(jQuery);
-
 
 
 
