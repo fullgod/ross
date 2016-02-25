d02b6d54b374dcbc26a5a79f08d413deb8261e50
fullgod <fullgod@gmail.com>
2016-02-25 16:07:40 +0300

    123

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -1,43 +1,33 @@
-(function($){ 
-    $(document).ready(function(){
-  
-		$('ul .dropdown-toggle[data-toggle=dropdown]').on('click', function(event) {
-       
-        var menu = $(this).parent().find('.dropdown-menu');
-        
-        //var menu = $(this).parent().find("ul");
-        
-        if (menu.hasClass('pull-center')) {
-            menu.css('margin-right', menu.outerWidth() / -2);
-        } 
-        if (menu.hasClass('pull-center')) {
-            menu.css('margin-right', menu.outerWidth() / -2);
-        } 
-        
-        
-        console.log("margin-right:", menu.outerWidth() / -2);
-        console.log("menu.offset:", menu.offset());
-        console.log("menu.width:", menu.width());
-        
+(function ($) {
 
-		});
+    $(document).ready(function () {
+
+        $('ul .dropdown-toggle[data-toggle=dropdown]').on('click', function (event) {
+
+            var menu = $(this).parent().find('.dropdown-menu');
+            //var menu = $(this).parent().find("ul");
         
-        
-        
-        
-	
-	});
-    
-    
-    
-$(document).ready(function(){
-            $('ul .dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
-            event.preventDefault(); 
-            event.stopPropagation(); 
+            if (menu.hasClass('pull-center')) {
+                menu.css('margin-right', menu.outerWidth() / -2);
+            }
+
+            console.log("margin-right:", menu.outerWidth() / -2);
+            console.log("menu.offset:", menu.offset());
+            console.log("menu.width:", menu.width());
+
+        });
+
+    });
+
+    $(document).ready(function () {
+        $('ul .dropdown-menu [data-toggle=dropdown]').on('click', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
             $(this).parent().siblings().removeClass('open');
             $(this).parent().toggleClass('open');
-            });
-            });
+        });
+    });
+
 })(jQuery);
 
 	
